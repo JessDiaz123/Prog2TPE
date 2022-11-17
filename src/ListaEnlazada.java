98cbd8cb262db246ec002942182c9e9539a8b753
Jesus Diaz <jesusdiaz013@gmail.com>
2022-11-17 10:05:12 -0300

    funcinando agregar ordenado para String e Int

--- src/ListaEnlazada.java
+++ src/ListaEnlazada.java
@@ -13,27 +13,85 @@ public class ListaEnlazada<T> {
     Nodo<T> nuevo = new Nodo<T>(datoNuevo);
     Nodo<T> puntero1;
     Nodo<T> puntero2;
-    Nodo<T> aux;
-    boolean flag = false;
     // si el primer nodo esta vacio
-    // while(flag!=false){
     if (primerNodo == null) {
       primerNodo = nuevo;
       primerNodo.setSiguiente(null);
-      flag = true;
     } else {
       puntero1 = primerNodo;
       while (puntero1 != null) {
         puntero2 = puntero1.getSiguiente();
+
+
         // el numero nuevo debe ir al inicio de la lista?
         // si el numero ingresado es mas grande que la raiz, pasa a ser la raiz
         if (orden.compare(puntero1.getValor(), nuevo.getValor()) >= 0) {
           nuevo.setSiguiente(primerNodo);
           primerNodo = nuevo;
           break;
+        }
+
+
+        // el numero nuevo va al final de la lista?
+        //por medio del while va a ir crecio este puntero1, hasta llegar al final
+        else if (
+          orden.compare(nuevo.getValor(), puntero1.getValor()) > 0 &&
+          puntero2 == null
+        ) {
+          puntero1.setSiguiente(nuevo);
+          break;
+        } 
+
+
+        // el nuevo tiene que ir entre 2 nodos?
+        else if (
+          (orden.compare(nuevo.getValor(), puntero1.getValor()) > 0) &&
+          orden.compare(puntero2.getValor(), nuevo.getValor()) > 0
+        ) {
+          puntero1.setSiguiente(nuevo);
+          nuevo.setSiguiente(puntero2);
+          break;
+        } 
+
+        
+        // si no, sigo recorriendo la lista hasta que puntero1 sea null
+        else {
+          puntero1 = puntero1.getSiguiente();
+        }
+      }
+    }
+  }
+
+  public String toString() {
+    Nodo<T> puntero = primerNodo;
+    String aux = "";
+    while (puntero != null) {
+      aux += puntero.getValor() + ",";
+      puntero = puntero.getSiguiente();
+    }
+    return aux;
+  }
+}
+/*
+ public void agregarOrdenado(T datoNuevo) {
+    Nodo<T> nuevo = new Nodo<T>(datoNuevo);
+    Nodo<T> puntero1;
+    Nodo<T> puntero2;
+    // si el primer nodo esta vacio
+    // while(flag!=false){if (primerNodo == null) {
+      primerNodo = nuevo;
+      primerNodo.setSiguiente(null);
+    } else {
+      puntero1 = primerNodo;
+      while (puntero1 != null) {
+        puntero2 = puntero1.getSiguiente();
+        // el numero nuevo debe ir al inicio de la lista?
+        if (orden.compare(puntero1.getValor(), nuevo.getValor()) >= 0) {
+          nuevo.setSiguiente(primerNodo);
+          primerNodo = nuevo;
+          break;
         } else {
           // el numero nuevo va al final de la lista?
-          //por medio del while va a ir crecio este puntero1, hasta llegar al final
           if (orden.compare(nuevo.getValor(), puntero1.getValor()) > 0 && puntero2 == null) {
             puntero1.setSiguiente(nuevo);
             break;
@@ -51,38 +109,4 @@ public class ListaEnlazada<T> {
         }
       }
     }
-  }
-  /*
-   * else if((orden.compare(primerNodo.getValor(), nuevo.getValor()) > 0)){
-   * aux = primerNodo;
-   * primerNodo=nuevo;
-   * primerNodo.setSiguiente(aux);
-   * flag = true;
-   * }
-   * else if(orden.compare(nuevo.getValor(),primerNodo.getValor()) > 0){
-   * puntero = primerNodo;
-   * while(orden.compare(nuevo.getValor(),puntero.getValor()) > 0 &&
-   * puntero.getSiguiente()!=null){
-   * puntero=puntero.getSiguiente();
-   * System.out.println("puntero vale:"+ puntero.getValor());
-   * }
-   * if(puntero.getSiguiente()==null){
-   * puntero.
-   * }
-   * aux = puntero.getSiguiente();
-   * primerNodo.setSiguiente(nuevo);
-   * nuevo.setSiguiente(aux);
-   * }
-   * 
-   */
-
-  public String toString() {
-    Nodo<T> puntero = primerNodo;
-    String aux = "";
-    while (puntero != null) {
-      aux += puntero.getValor() + ",";
-      puntero = puntero.getSiguiente();
-    }
-    return aux;
-  }
-}
+ */
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
 import java.util.Comparator;
 
 import Comparadores.ComparadorInt;
+import Comparadores.ComparadorString;
 
 public class Main {
     public static void main(String[] args) {
         
-        ComparadorInt c1 = new ComparadorInt();
+        ComparadorString c2 = new ComparadorString();
         
-        ListaEnlazada<Integer> l1 = new ListaEnlazada<>(c1);
+        
+        
+        //String
+        ListaEnlazada<String> listaString = new ListaEnlazada<>(c2);
 
-        l1.agregarOrdenado(16);
-        l1.agregarOrdenado(2);
-        l1.agregarOrdenado(17);
-        l1.agregarOrdenado(7);
-        l1.agregarOrdenado(4);
+        listaString.agregarOrdenado("Parcial");
+        listaString.agregarOrdenado("Facil");
+        listaString.agregarOrdenado("Es");
+        listaString.agregarOrdenado("Prog2");
 
-        System.out.println(l1);
+        System.out.println(listaString);
+
+        //int 
+        // ComparadorInt c1 = new ComparadorInt();
+        // ListaEnlazada<Integer> l1 = new ListaEnlazada<>(c1);
+        // l1.agregarOrdenado(16);
+        // l1.agregarOrdenado(2);
+        // l1.agregarOrdenado(17);
+        // l1.agregarOrdenado(7);
+        // l1.agregarOrdenado(4);
+
+        // System.out.println(l1);
 
     }
 }
